800ef91a4229055b2f1dda70a076931f29387854
artur kipnis <artur@app-central.com>
2024-12-02 06:52:52 +0200

    added exc handler

--- app/src/main/java/ai/arturxdroid/contacts/presentation/ContactsViewModel.kt
+++ app/src/main/java/ai/arturxdroid/contacts/presentation/ContactsViewModel.kt
@@ -2,10 +2,12 @@ package ai.arturxdroid.contacts.presentation
 
 import ai.arturxdroid.contacts.data.Contact
 import ai.arturxdroid.contacts.data.ContactsRepository
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
@@ -15,19 +17,28 @@ class ContactsViewModel(private val repository: ContactsRepository) : ViewModel(
         MutableLiveData(listOf<Contact>())
     val contactsLiveData: LiveData<List<Contact>> = internalContactLiveData
 
+    private val loggingExceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+        Log.e(
+            "Coroutine exception",
+            throwable.message,
+            throwable
+        )
+    }
+
 
     fun fetchContacts() {
         viewModelScope.launch {
-            val contactsAsync = async { repository.getPhoneContacts() }
-            val numbersAsync = async { repository.getContactNumbers() }
+            val contactsAsync = async(loggingExceptionHandler) { repository.getPhoneContacts() }
+            val numbersAsync = async(loggingExceptionHandler) { repository.getContactNumbers() }
 
             val contacts = contactsAsync.await()
-            val imagesAsync = async { repository.getContactsImage(contacts.map { contact -> contact.id.toLong() }) }
+            val imagesAsync =
+                async(loggingExceptionHandler) { repository.getContactsImage(contacts.map { contact -> contact.id.toLong() }) }
 
             val images = imagesAsync.await()
             val numbers = numbersAsync.await()
 
-            contacts.forEach{ contact ->
+            contacts.forEach { contact ->
                 numbers[contact.id]?.let { number ->
                     contact.number = number
                 }
